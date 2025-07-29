e871af2bfacc7920a7cef6fbc5fe9dc5118fe2e3
bisma <gyndarab@gmail.com>
2025-07-30 01:10:02 +0700

    main controller

---
+++ bazff/src/bazff/controller/MainWindowController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bazff.controller;
+
+import bazff.view.MainWindow;
+import bazff.view.ProductDataPanel;
+
+/**
+ *
+ * @author bisma
+ */
+public class MainWindowController {
+    private ProductDataPanel ProductDataPanel;
+
+    public void setProductDataPanel(ProductDataPanel ProductDataPanel) {
+        this.ProductDataPanel = ProductDataPanel;
+    }
+    
+    
+    public void tampilHalamanProduct(MainWindow window){
+        //mengubah panel header
+        window.getPanelHeader().removeAll();
+        window.getPanelHeader().repaint();;
+        window.getPanelHeader().revalidate();
+        //mengubah panel main
+        window.getMainPanel().removeAll();
+        window.getMainPanel().repaint();
+        window.getMainPanel().revalidate();
+        
+        window.getPanelHeader().add(ProductDataPanel.getPanelHeaderProduct());
+        window.getPanelHeader().repaint();
+        window.getPanelHeader().revalidate();
+        
+        window.getMainPanel().add(ProductDataPanel.getPanelProductData());
+        window.getMainPanel().repaint();
+        window.getMainPanel().revalidate();
+    }
+}
