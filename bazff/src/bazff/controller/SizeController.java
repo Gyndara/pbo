6b149b397a17b74e27e15f569e367249c6c485cb
bisma <gyndarab@gmail.com>
2025-08-02 17:09:59 +0700

    add controller size

---
+++ bazff/src/bazff/controller/SizeController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bazff.controller;
+
+import bazff.view.MainWindow;
+import bazff.view.SizePopUp1;
+
+/**
+ *
+ * @author bisma
+ */
+public class SizeController {
+    private MainWindow mainWindow;
+    private SizePopUp1 sizePopUp1;
+    
+    public SizeController(MainWindow mainWindow){
+        this.mainWindow = mainWindow;
+    }
+    
+    public void addSizeDataPopUp(){
+        sizePopUp1 = new SizePopUp1(mainWindow, this);
+        sizePopUp1.setLocationRelativeTo(mainWindow);
+        sizePopUp1.setVisible(true);
+    }
+}
