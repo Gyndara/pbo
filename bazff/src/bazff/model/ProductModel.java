32304d9d15461ad75dac85945824aac3f3baf0dd
bisma <gyndarab@gmail.com>
2025-08-03 22:27:11 +0700

    model for product display

---
+++ bazff/src/bazff/model/ProductModel.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bazff.model;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author bisma
+ */
+public class ProductModel {
+    private String productCode;
+    private String productName;
+    private String skuCode;
+    private int quantity;
+    private String productStatus;
+    
+    public ProductModel(){
+        
+    }
+    
+    public ProductModel(String productCode, String productName, String skuCode, int quantity, String productStatus) {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.skuCode = skuCode;
+        this.quantity = quantity;
+        this.productStatus = productStatus;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getSkuCode() {
+        return skuCode;
+    }
+
+    public void setSkuCode(String skuCode) {
+        this.skuCode = skuCode;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(String productStatus) {
+        this.productStatus = productStatus;
+    }
+    
+    
+}
