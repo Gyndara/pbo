0e57edde44962b24ef210e4fb55f209fc81d832d
bisma <gyndarab@gmail.com>
2025-08-03 22:24:26 +0700

    Connecting database

--- bazff/src/bazff/config/Database.java
+++ bazff/src/bazff/config/Database.java
@@ -5,10 +5,30 @@
  */
 package bazff.config;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  *
  * @author bisma
  */
 public class Database {
-    
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/point_of_sales";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "";
+    private static Connection koneksi;
+     
+    public static Connection getKoneksi() throws SQLException{
+        if (koneksi == null){
+            MysqlDataSource mysqlDS = new MysqlDataSource();
+            
+            mysqlDS.setURL(DB_URL);
+            mysqlDS.setUser(DB_USERNAME);
+            mysqlDS.setPassword(DB_PASSWORD);
+            
+            koneksi = mysqlDS.getConnection();
+        }
+        return koneksi;
+    }
 }
