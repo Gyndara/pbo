e2e434c8626a8cc431754889b02258994e64518e
bisma <gyndarab@gmail.com>
2025-08-03 22:25:23 +0700

    setup showing data from database into table

--- bazff/src/bazff/controller/TableController.java
+++ bazff/src/bazff/controller/TableController.java
@@ -5,12 +5,19 @@
  */
 package bazff.controller;
 
+import bazff.config.Database;
+import bazff.dao.ProductDAO;
+import bazff.model.ProductModel;
 import bazff.view.ProductDataPanel;
 import bazff.view.SizeDataView;
 import bazff.view.TransactionDataPanel;
 import java.awt.Font;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 
 /**
@@ -21,6 +28,7 @@ public class TableController {
     private SizeDataView sizeDataView;
     private ProductDataPanel productDataPanel;
     private TransactionDataPanel transactionDataPanel;
+    private ProductController productController;
     
     
     public TableController(ProductDataPanel productDataPanel){
@@ -49,16 +57,40 @@ public class TableController {
     }
     
     public void setUpTableProduct(){
+        try {
+            Connection conn = Database.getKoneksi();
+            ProductDAO productDAO = new ProductDAO(conn);
+            List<ProductModel> produkList = productDAO.getProductDisplay();
+
+            DefaultTableModel model = (DefaultTableModel) productDataPanel.getTableProduct().getModel();
+            model.setRowCount(0); // Bersihkan isi tabel sebelum diisi ulang
+
+            for (ProductModel p : produkList) {
+                model.addRow(new Object[]{
+                    p.getProductCode(),
+                    p.getSkuCode(),
+                    p.getProductName(),
+                    p.getQuantity(),
+                    p.getProductStatus()
+                });
+            }
+
+        // Center align isi tabel
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
         centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
 
         for (int i = 0; i < productDataPanel.getTableProduct().getColumnCount(); i++) {
             productDataPanel.getTableProduct().getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
         }
-        
+
+        // Styling tambahan
         productDataPanel.getTableProduct().setRowHeight(60);
         JTableHeader header = productDataPanel.getTableProduct().getTableHeader();
-        header.setFont(new Font("Tahome", Font.BOLD, 20));
+        header.setFont(new Font("Tahoma", Font.BOLD, 20));
+
+        } catch (SQLException e) {
+            e.printStackTrace(); // atau tampilkan di UI
+        }
     }
     
     public void setUpTableTransaction(){
